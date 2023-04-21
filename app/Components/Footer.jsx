f8b924afdb9e9d39838155107801c6f8379098c5
Galpovsod8 <galpos10@gmail.com>
2023-04-21 11:29:39 +0200

    updated settings

--- app/Components/Footer.jsx
+++ app/Components/Footer.jsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 
 export default function Footer() {
   return (
-    <footer className="lg:absolute bottom-0 h-max lg:h-44 w-full flex items-center justify-center lg:gap-0 gap-5 flex-col py-10 lg:py-0 animate-show bg-mainDark text-whiteColor">
+    <footer className="lg:absolute font-Montserrat bottom-0 h-max lg:h-44 w-full flex items-center justify-center lg:gap-0 gap-5 flex-col py-10 lg:py-0 animate-show bg-mainDark text-whiteColor">
         <div className="h-full w-4/5 lg:w-3/5 flex items-center justify-between flex-col lg:flex-row text-xl font-normal lg:gap-4 gap-10">
             <Link href="mailto:info@elektriada-2023.si" className="grid place-items-center w-full">info@elektriada-2023.si</Link>
             <div className="lg:h-full lg:w-1 h-px w-full bg-whiteColor"></div>
--- app/page.jsx
+++ app/page.jsx
@@ -2,13 +2,13 @@ import BtnPrimary from "./Components/BtnPrimary"
 
 export default function Home() {
   return (
-    <main className="lg:h-screen h-[80vh] w-full grid place-items-center bg-mainDark text-whiteColor">
-      <article className="absolute top-36 lg:top-44 h-max w-full flex flex-col items-center justify-between gap-4 lg:gap-20">
+    <main className="lg:h-screen h-[70vh] w-full grid place-items-center font-Montserrat bg-mainDark text-whiteColor">
+      <article className="absolute top-44 lg:top-44 h-max w-full flex flex-col items-center justify-between gap-14 lg:gap-20">
         <h1 className="lg:text-9xl md:text-7xl text-5xl text-center font-semibold animate-fadeIn">ELEKTRIADA <span className="text-primaryColor">2023</span></h1>
         <div className="grid place-items-center gap-8 animate-show">
           <div className="text-2xl lg:text-4xl text-center grid place-items-center gap-4">
-            <h2>APRIL 17-18</h2>
-            <h2 className=" font-medium">UMAG, HRAVAŠKA</h2>
+            <h2>OKTOBER 6 - 8</h2>
+            <h2 className=" font-medium">UMAG, HRVAŠKA</h2>
           </div>
           <BtnPrimary BtnPrimaryText="HOTEL SOL GARDEN ISTRA" BtnPrimaryLink="https://www.plavalaguna.com/sl/hoteli/garden-istra" BtnPrimaryTarget="blank"/>
         </div>
